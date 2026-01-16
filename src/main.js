5fd7a49b56192d8bad706d4e896fbc56232f46b4
kimpe <Ibe.kimpe@student.howest.be>
2026-01-16 14:54:43 +0100

    First Puzzel

--- src/main.js
+++ src/main.js
@@ -1,32 +1,16 @@
 import './style.css'
-
-let currentDemo = '';
+import './puzzle.js'
 
 function showDemo(demoNumber) {
-  currentDemo = demoNumber;
-  const contentDiv = document.querySelector('#content');
-  
-  if (demoNumber === 1) {
-    contentDiv.innerHTML = '<h1>Puzzel</h1>';
-  } else if (demoNumber === 2) {
-    contentDiv.innerHTML = '<h1>Lamp</h1>';
-  } else if (demoNumber === 3) {
-    contentDiv.innerHTML = '<h1>Tactiek</h1>';
+  for (let i = 1; i <= 3; i++) {
+    const section = document.querySelector(`#demo-section-${i}`);
+    if (i === demoNumber) {
+      section.classList.remove('hidden');
+    } else {
+      section.classList.add('hidden');
+    }
   }
 }
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="button-container">
-      <button id="demo1">Demo 1</button>
-      <button id="demo2">Demo 2</button>
-      <button id="demo3">Demo 3</button>
-    </div>
-    <div id="content">
-      <h1>Hello World</h1>
-    </div>
-  </div>
-`
 
 document.querySelector('#demo1').addEventListener('click', () => showDemo(1));
 document.querySelector('#demo2').addEventListener('click', () => showDemo(2));
